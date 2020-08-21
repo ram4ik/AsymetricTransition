ca0886e52d69d670a0852a96dc58f6f1a31b4054
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-21 22:36:30 +0300

    init

--- AsymetricTransition/ContentView.swift
+++ AsymetricTransition/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var show: Bool = false
+    @State private var transform: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if self.show {
+                Rectangle()
+                    .frame(width: self.transform ? 250 : 300, height: self.transform ? 250 : 300)
+                    .foregroundColor(self.transform ? .orange : .pink)
+                    .cornerRadius(self.transform ? 50 : 0)
+                    .transition(
+                        .asymmetric(
+                            insertion: AnyTransition
+                                        .opacity
+                                        .animation(Animation.spring().delay(0.3)),
+                                        
+                            removal: AnyTransition
+                                        .opacity
+                                        .animation(.spring())
+                        )
+                    )
+                    .onTapGesture {
+                        withAnimation {
+                            self.transform.toggle()
+                        }
+                }
+            }
+            
+            Button(action: {
+                withAnimation {
+                    self.show.toggle()
+                }
+            }) {
+                Text("Toggle")
+                    .foregroundColor(.black)
+                    .bold()
+                    .font(.largeTitle)
+            }
+        }
     }
 }
 
